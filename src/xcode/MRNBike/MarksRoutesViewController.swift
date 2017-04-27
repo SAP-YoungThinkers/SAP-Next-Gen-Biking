c6bd2d660fab8a660551cd1be61b0f120637627f
loberste <huyen@Huyens-MBP.fritz.box>
2017-04-27 14:05:37 +0200

    Tab Bar finish
    Tab Bar with redrawing while orientation change, map centered and all
    looks fine :)

--- src/xcode/MRNBike/MarksRoutesViewController.swift
+++ src/xcode/MRNBike/MarksRoutesViewController.swift
@@ -14,14 +14,19 @@ class MarksRoutesViewController: UIViewController {
     @IBOutlet weak var topBar: UITabBar!
     @IBOutlet weak var routeInformation: UITabBarItem!
     @IBOutlet weak var myRoutes: UITabBarItem!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let config = Configurator()
+    
+    var locationManager = LocationManager()
+    var tempPlaceholder : UIView?
     
     let primaryColor = UIColor(red: (192/255.0), green: (57/255.0), blue: (43/255.0), alpha: 1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // display line on selected
-        topBar.selectionIndicatorImage = UIImage().createSelectionIndicator(color: primaryColor, size: CGSize(width: topBar.frame.width/CGFloat(topBar.items!.count), height: topBar.frame.height), lineWidth: 3.0)
+        self.mapView.showsUserLocation = false
         
         // Remove Grey Lines by initialising empty Image
         topBar.shadowImage = UIImage()
@@ -34,29 +39,85 @@ class MarksRoutesViewController: UIViewController {
             item.titlePositionAdjustment = UIOffset.init(horizontal: 0, vertical: -17)
         }
         
-        // add seperator
-        setupTabBarSeparators()
+        // fix size errors
+        topBar.bounds.size.width = UIScreen.main.bounds.width
+        topBar.itemWidth = CGFloat(topBar.bounds.size.width/CGFloat(topBar.items!.count))
+        topBar.updateConstraints()
         
+        // display line on selected
+        topBar.selectionIndicatorImage = UIImage().createSelectionIndicator(color: primaryColor, size: CGSize(width: topBar.frame.width/CGFloat(topBar.items!.count), height: topBar.frame.height), lineWidth: 3.0)
+        
+        // add seperator
+        tempPlaceholder = setupTabBarSeparators()
+        
+        // center map around position
+        let centerPoint = getPosition()
+        centerMap(centerPoint: centerPoint)
     }
     
+    func getPosition() -> CLLocationCoordinate2D {
+        return locationManager.center
+    }
     
-    func setupTabBarSeparators() {
-        let itemWidth = floor(self.topBar.frame.size.width / CGFloat(self.topBar.items!.count))
+    func centerMap(centerPoint: CLLocationCoordinate2D){
+        let region = MKCoordinateRegion(center: centerPoint, span: MKCoordinateSpan(latitudeDelta: config.zoomLevel, longitudeDelta: config.zoomLevel))
+        
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        coordinator.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            
+            let orient = UIApplication.shared.statusBarOrientation
+            
+            switch orient {
+            case .portrait:
+                print("Portrait")
+            default:
+                print("Anything But Portrait")
+            }
+            
+        }, completion: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            // on device orientation change
+            
+            // update lines
+            self.updateTapBar()
+        })
+        
+        super.viewWillTransition(to: size, with: coordinator)
+    }
+    
+    func updateTapBar() {
+        
+        // remove seperators and images
+        self.tempPlaceholder?.removeFromSuperview()
+        topBar.selectionIndicatorImage = UIImage()
+        
+        // add again
+        self.tempPlaceholder? = setupTabBarSeparators()
+        topBar.selectionIndicatorImage = UIImage().createSelectionIndicator(color: primaryColor, size: CGSize(width: topBar.frame.width/CGFloat(topBar.items!.count), height: topBar.frame.height), lineWidth: 3.0)
+        
+    }
+
+    
+    func setupTabBarSeparators() -> UIView {
+        let itemWidth = floor(self.topBar.frame.width / CGFloat(self.topBar.items!.count))
         
         // this is the separator width.  0.5px matches the line at the top of the tab bar
         let separatorWidth: CGFloat = 0.5
         
-        // iterate through the items in the Tab Bar, except the last one
-        for i in 0...(self.topBar.items!.count - 1) {
-            
+        
             // make a new separator at the end of each tab bar item
-            let separator = UIView(frame: CGRect(x: itemWidth * CGFloat(i) + 0.5 - CGFloat(separatorWidth / 2), y: 0, width: CGFloat(separatorWidth), height: self.topBar.frame.size.height))
+            let separator = UIView(frame: CGRect(x: itemWidth + 0.5 - CGFloat(separatorWidth / 2), y: 0, width: CGFloat(separatorWidth), height: self.topBar.frame.height))
             
             // set the color to light gray (default line color for tab bar)
             separator.backgroundColor = UIColor(red: (170/255.0), green: (170/255.0), blue: (170/255.0), alpha: 1.0)
             
-            self.topBar.addSubview(separator)
-        }
+        self.topBar.insertSubview(separator, at: 1)
+        
+        
+        return separator
     }
     
 }
