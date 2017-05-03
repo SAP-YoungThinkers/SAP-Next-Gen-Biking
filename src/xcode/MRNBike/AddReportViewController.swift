28fdb2f641a0244be9531d9695ea0dc7f5ddef8b
Bartosz Wilkusz <bartosz@bartoszs-mac.mshome.net>
2017-05-03 22:05:10 +0200

    Design and Layout finished

--- src/xcode/MRNBike/AddReportViewController.swift
+++ src/xcode/MRNBike/AddReportViewController.swift
@@ -10,25 +10,30 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class AddReportViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class AddReportViewController: UIViewController, UITextFieldDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
 
     //MARK: Properties
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var messageBox: UITextView!
+    @IBOutlet weak var messageBox: UITextField!
+    @IBOutlet weak var messageView: UIView!
     
-    var lastUserLocation: MKUserLocation?
+    //Radio buttons
     
-    var locationManager = CLLocationManager()
+    @IBOutlet weak var recommendationBtn: RadioButtonClass!
+    
+    @IBOutlet weak var warningBtn: RadioButtonClass!
+    
+    @IBOutlet weak var dangerBtn: RadioButtonClass!
+    
     let manager = CLLocationManager()
     let regionRadius: CLLocationDistance = 1000
-    
-    let button1 = RadioButtonClass(frame: CGRect(x: 20, y: 170, width: 50, height: 50))
-    let button2 = RadioButtonClass(frame: CGRect(x: 20, y: 350, width: 50, height: 50))
-    let button3 = RadioButtonClass(frame: CGRect(x: 20, y: 450, width: 50, height: 50))
+
     
     //MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        messageBox.delegate = self
         
         //Setup location manager and properties
         manager.delegate = self
@@ -36,38 +41,42 @@ class AddReportViewController: UIViewController, MKMapViewDelegate, CLLocationMa
         manager.requestAlwaysAuthorization()
         manager.startUpdatingLocation()
         
-        //Create buttons for custom radio buttons
-        button1.addTarget(self, action: #selector(manualAction(sender:)), for: .touchUpInside)
-        self.view.addSubview(button1)
-        button1.isSelected = true
-        button1.tag = 1
+        //Settings from the radio buttons
+        recommendationBtn.addTarget(self, action: #selector(manualAction(sender:)), for: .touchUpInside)
+        self.view.addSubview(recommendationBtn)
+        recommendationBtn.isSelected = true
+        recommendationBtn.tag = 1
         
-        button2.addTarget(self, action: #selector(manualAction(sender:)), for: .touchUpInside)
-        self.view.addSubview(button2)
-        button2.isSelected = false
-        button2.tag = 2
+        warningBtn.addTarget(self, action: #selector(manualAction(sender:)), for: .touchUpInside)
+        self.view.addSubview(warningBtn)
+        warningBtn.isSelected = false
+        warningBtn.tag = 2
         
-        button3.addTarget(self, action: #selector(manualAction(sender:)), for: .touchUpInside)
-        self.view.addSubview(button3)
-        button3.isSelected = false
-        button3.tag = 3
+        dangerBtn.addTarget(self, action: #selector(manualAction(sender:)), for: .touchUpInside)
+        self.view.addSubview(dangerBtn)
+        dangerBtn.isSelected = false
+        dangerBtn.tag = 3
         
         //Setup gesture recognizer for long press recognition
         let gestureRecognizer = UILongPressGestureRecognizer(target: self, action:(#selector(longPress)))
         gestureRecognizer.minimumPressDuration = 2.0
         gestureRecognizer.delegate = self as? UIGestureRecognizerDelegate
         mapView.addGestureRecognizer(gestureRecognizer)
+        
+        //Border from the bottom background view
+        self.messageView.layer.borderColor = UIColor.gray.cgColor
+        self.messageView.layer.borderWidth = 1
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        let message: String = String(messageBox.text)
+        let message: String = messageBox.text!
         var type : String
         
-        if button1.isSelected {
+        if recommendationBtn.isSelected {
             type = "Recommendation"
-        } else if button2.isSelected {
+        } else if warningBtn.isSelected {
             type = "Warning"
         } else {
             type = "Dangerousness"
@@ -95,7 +104,7 @@ class AddReportViewController: UIViewController, MKMapViewDelegate, CLLocationMa
             let region = MKCoordinateRegionMake(myLocation, span)
             mapView.setRegion(region, animated: true)
         }
-        self.mapView.showsUserLocation = true
+        //self.mapView.showsUserLocation = true
         manager.stopUpdatingLocation()
     }
     
@@ -113,25 +122,34 @@ class AddReportViewController: UIViewController, MKMapViewDelegate, CLLocationMa
         switch sender.tag
         {
         case 1:
-            button1.isSelected = true
-            button2.isSelected = false
-            button3.isSelected = false
+            recommendationBtn.isSelected = true
+            warningBtn.isSelected = false
+            dangerBtn.isSelected = false
             break
         case 2:
-            button1.isSelected = false
-            button2.isSelected = true
-            button3.isSelected = false
+            recommendationBtn.isSelected = false
+            warningBtn.isSelected = true
+            dangerBtn.isSelected = false
             break
         case 3:
-            button1.isSelected = false
-            button2.isSelected = false
-            button3.isSelected = true
+            recommendationBtn.isSelected = false
+            warningBtn.isSelected = false
+            dangerBtn.isSelected = true
             break
         default: break
         }
     }
     
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ messageBox: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
     //MARK: Actions
+    
+    //Focus back on users location
     @IBAction func refreshLocation(_ sender: UIButton) {
         //Get actual user location
         manager.startUpdatingLocation()
