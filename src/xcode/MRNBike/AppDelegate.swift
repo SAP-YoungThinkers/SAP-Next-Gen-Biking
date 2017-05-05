a6f52269b4334debce882a2325fffb527487d43c
Ziad Abdelkader <zabdelka@mail.uni-mannheim.de>
2017-05-05 23:27:24 +0200

    - Added Tab Bar Controller -Added Items + Background Color + Separators - Add Icons as Assets - Divided HomeScreen into 5 Views - Added Weather view with Icons (Dummy Data) - Added Tracking button + Navigation to Tracking Storyboard - Added Profile Name + Picture (Dummy Data)

--- src/xcode/MRNBike/AppDelegate.swift
+++ src/xcode/MRNBike/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     //let firstVC = FirstViewController()
     
-let firstVC = MarksRoutesViewController()
+//     let firstVC = TabBarViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
