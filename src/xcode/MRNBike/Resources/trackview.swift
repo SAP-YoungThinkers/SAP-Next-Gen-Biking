59bc3b0950c25f2f5122baacd4d5d724c1f7c88e
YuanXU5166 <hack@seirradeMac.local>
2017-05-08 05:45:35 +0800

    Merged Tracking pages successfully. Problems need to solve: -when click back during tracking, hide navigation bar -when click when click save routes, show tab bar -when click report route, switch to add routes, when click done, back to save route page. ----Yuan XU

---
+++ src/xcode/MRNBike/Resources/trackview.swift
@@ -0,0 +1,18 @@
+//
+//  trackview.swift
+//  MRNBike
+//
+//  Created by seirra on 2017/5/5.
+//  Copyright © 2017年 Marc Bormeth. All rights reserved.
+//
+
+import UIKit
+
+class trackview : UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
