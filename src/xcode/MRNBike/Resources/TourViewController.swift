74bb7626f16ee79d3bf290d3ef1f3a84918b8564
labdulli <aluba195@mail.ru>
2017-05-15 00:27:15 +0200

    Video can be played. Location of elements should be fixed

--- src/xcode/MRNBike/Resources/TourViewController.swift
+++ src/xcode/MRNBike/Resources/TourViewController.swift
@@ -11,8 +11,28 @@ import UIKit
 class TourViewController: UIViewController {
     var startRidingAction: (() -> ())?
     
-    @IBAction func startRidingPressed(_ sender: Any) {
+   /* @IBAction func startRidingPressed(_ sender: Any) {
         startRidingAction?()
         self.close()
-  }
+  }*/
+    
+    
+    @IBOutlet weak var wv: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadYoutube(videoID: "7iT9fueKCJM")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    func loadYoutube(videoID:String) {
+        guard
+            let youtubeURL = URL(string: "https://www.youtube.com/embed/\(videoID)")
+            else { return }
+        wv.loadRequest( URLRequest(url: youtubeURL) ) 
+    }
+    
+    
 }
