de321b5b7cdff6aeb14b6cec4e2cdcfcf30c70a2
loberste <oberste@outlook.de>
2017-05-20 22:31:17 +0200

    Checks if last time login still valid
    ToDo: backend check current login

--- src/xcode/MRNBike/Resources/StartViewController.swift
+++ src/xcode/MRNBike/Resources/StartViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class StartViewController: UIViewController {
     
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         self.navigationController?.isNavigationBarHidden = true
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let controller = segue.destination as? TourViewController {
+        
+        
+        // ------------------------------------------------------
+        // TODO: backend check the current login state!
+        // ------------------------------------------------------
+        
+        
+        let refreshAlert = UIAlertController(title: "login last time", message: "Assume: Were you already registered, and is the login still valid?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // if login is TRUE -> DASHBOARD
+        refreshAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action: UIAlertAction!) in
+            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+            let controller = storyboard.instantiateViewController(withIdentifier: "Home")
+            self.present(controller, animated: true, completion: nil)
+        }))
+        
+        // if login is FALSE -> SIGN IN PAGE
+        refreshAlert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { (action: UIAlertAction!) in
+            let controller = TourViewController()
             controller.startRidingAction = {
                 self.performSegue(withIdentifier: "segSignIn", sender: self)
             }
-   
+        }))
+        
+        present(refreshAlert, animated: true, completion: nil)
     }
 
-}
 }
