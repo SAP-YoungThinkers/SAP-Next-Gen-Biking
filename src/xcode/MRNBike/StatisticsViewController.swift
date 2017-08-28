fd961183f65ee8dc39d295e828950304b9a60824
loberste <oberste@outlook.de>
2017-08-28 15:35:09 +0200

    view controller init

---
+++ src/xcode/MRNBike/StatisticsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StatisticsViewController.swift
+//  MRNBike
+//
+//  Created by Huyen Nguyen on 28.08.17.
+//
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.parent?.title = "asdasd"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
