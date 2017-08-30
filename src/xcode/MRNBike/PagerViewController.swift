0aad5839342be8f734b7517281f06b8ab15ec042
loberste <oberste@outlook.de>
2017-08-30 10:28:23 +0200

    subview controller tabs

---
+++ src/xcode/MRNBike/PagerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PagerViewController.swift
+//  MRNBike
+//
+//  Created by Huyen Nguyen on 30.08.17.
+//
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class PagerViewController: ButtonBarPagerTabStripViewController {
+
+    @IBOutlet weak var containerView: UIScrollView!
+    
+
+}
