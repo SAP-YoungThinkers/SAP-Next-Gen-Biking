99a033bcddcdcf99f9bb032604c35c1da2b4b4b2
loberste <oberste@outlook.de>
2017-09-21 19:17:09 +0200

    social navbar fixes

--- src/xcode/MRNBike/SocialViewController.swift
+++ src/xcode/MRNBike/SocialViewController.swift
@@ -10,6 +10,7 @@ class SocialViewController: UIViewController, UITabBarDelegate {
     @IBOutlet weak var groupsContainer: UIView!
     
     var tempPlaceholder : UIView?
+    private var shadowImageView: UIImageView?
     let primaryColor = UIColor(red: (192/255.0), green: (57/255.0), blue: (43/255.0), alpha: 1.0)
     
     override func viewDidLoad() {
@@ -20,30 +21,21 @@ class SocialViewController: UIViewController, UITabBarDelegate {
         friendsBarItem.title = NSLocalizedString("frindsTabBarItem", comment: "")
         groupsBarItem.title = NSLocalizedString("groupsTabBarItem", comment: "")
         
+
+        /*  ------------------------ *\
+         *      DESIGN
+        \*  ------------------------ */
         tabBar.delegate = self
         tabBar.selectedItem = friendsBarItem
-        
-        // Remove Grey Lines by initialising empty Image
         tabBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        tabBar.selectionIndicatorImage = UIImage().createSelectionIndicator(color: primaryColor, size: CGSize(width: tabBar.frame.width/CGFloat(tabBar.items!.count), height: 60), lineWidth: 3.0)
+        tempPlaceholder = setupTabBarSeparators()
+        self.childViewControllers.first!.view.backgroundColor = UIColor.clear
         
         // Set Font, Size for Items
         for item in tabBar.items! {
             item.setTitleTextAttributes([NSFontAttributeName : UIFont.init(name: "Montserrat-Regular", size: 18) ?? UIFont.systemFont(ofSize: 18)], for: UIControlState.normal)
-            item.titlePositionAdjustment = UIOffset.init(horizontal: 0, vertical: -17)
         }
-        
-        // fix size errors
-        tabBar.bounds.size.width = UIScreen.main.bounds.width
-        tabBar.itemWidth = CGFloat(tabBar.bounds.size.width/CGFloat(tabBar.items!.count))
-        tabBar.updateConstraints()
-        
-        // display line on selected
-        tabBar.selectionIndicatorImage = UIImage().createSelectionIndicator(color: primaryColor, size: CGSize(width: tabBar.frame.width/CGFloat(tabBar.items!.count), height: tabBar.frame.height), lineWidth: 3.0)
-        
-        // add seperator
-        tempPlaceholder = setupTabBarSeparators()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -54,6 +46,11 @@ class SocialViewController: UIViewController, UITabBarDelegate {
         } else {
             loadGroupsContent()
         }
+        
+        if shadowImageView == nil {
+            shadowImageView = findShadowImage(under: navigationController!.navigationBar)
+        }
+        shadowImageView?.isHidden = true
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -83,30 +80,39 @@ class SocialViewController: UIViewController, UITabBarDelegate {
         groupsContainer.isHidden = false
     }
     
-    func updateTapBar() {
-        // remove seperators and images
-        self.tempPlaceholder?.removeFromSuperview()
-        tabBar.selectionIndicatorImage = UIImage()
-        
-        // add again
-        self.tempPlaceholder? = setupTabBarSeparators()
-        tabBar.selectionIndicatorImage = UIImage().createSelectionIndicator(color: primaryColor, size: CGSize(width: tabBar.frame.width/CGFloat(tabBar.items!.count), height: tabBar.frame.height), lineWidth: 3.0)
+    override func viewWillLayoutSubviews() {
+        var tabFrame = self.tabBar.frame
+        tabFrame.size.height = 60
+        self.tabBar.frame = tabFrame
+        self.tabBar.updateConstraints()
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        shadowImageView?.isHidden = false
+    }
+    
     func setupTabBarSeparators() -> UIView {
         let itemWidth = floor(self.tabBar.frame.width / CGFloat(self.tabBar.items!.count))
-        
-        // this is the separator width.  0.5px matches the line at the top of the tab bar
         let separatorWidth: CGFloat = 0.5
-        
-        // make a new separator at the end of each tab bar item
-        let separator = UIView(frame: CGRect(x: itemWidth + 0.5 - CGFloat(separatorWidth / 2), y: 0, width: CGFloat(separatorWidth), height: self.tabBar.frame.height))
-        
-        // set the color to light gray (default line color for tab bar)
+        let separator = UIView(frame: CGRect(x: itemWidth + 0.5 - CGFloat(separatorWidth / 2), y: 0, width: CGFloat(separatorWidth), height: 60))
         separator.backgroundColor = UIColor(red: (170/255.0), green: (170/255.0), blue: (170/255.0), alpha: 1.0)
-        
         self.tabBar.insertSubview(separator, at: 1)
         
         return separator
+    }
+    
+    private func findShadowImage(under view: UIView) -> UIImageView? {
+        if view is UIImageView && view.bounds.size.height <= 1 {
+            return (view as! UIImageView)
+        }
+        
+        for subview in view.subviews {
+            if let imageView = findShadowImage(under: subview) {
+                return imageView
+            }
+        }
+        return nil
     }
 }
